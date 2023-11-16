34f5a5d8e815c3ab1645c90b375bb4a62e6b69cb
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-16 20:24:22 +0200

    fixing paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 import AuthNav from "./components/Header/AuthNav";
 import Header from "./components/Header/Header";
@@ -10,8 +10,9 @@ import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import Catalog from "./components/Catalog/Catalog";
 import Create from "./components/Create/Create";
-import Details from "./components/Details/Details"
-import Loading from "./components/Loading/Loading"
+import Details from "./components/Details/Details";
+import Edit from "./components/Edit/Edit";
+import Loading from "./components/Loading/Loading";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -35,13 +36,13 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/create" element={<Create />} />
-         <Route path='/details/:bookID' element={<Details />} />
+          <Route path="/details/:bookID" element={<Details />} />
+          <Route path="/details/:bookID/edit" element={<Edit />} />
 
           {/* <Route path='/contact-us' element={<ContactUs/>} />
          <Route path='/about' element={<About />} />
          <Route path='/not-found' element={<NotFound />} />
          <Route path='/search' element={<Search />} />
-         <Route path='/books/edit' element={<Edit />} />
          <Route path='*' element={<NotFound />} />    */}
         </Routes>
       </main>
