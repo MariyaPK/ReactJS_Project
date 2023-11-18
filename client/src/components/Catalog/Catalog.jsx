7aa59f7a23b8c7e9aa64bb39bc952af0dec3157a
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-18 14:35:24 +0200

    fixing catalog

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -2,26 +2,46 @@ import styles from "./Catalog.module.css";
 import { useState, useEffect } from "react";
 import * as bookService from "../../services/bookService";
 import CatalogItem from "./CatalogItem";
+import Loading from "../Loading/Loading";
 
 
 export default function Catalog() {
   const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    bookService.getAll().then((result) => {
-      // console.log(result);
-      setBooks(result);
-    });
+    const fetchData = async () => {
+      try {
+        const result = await bookService.getAll();
+        setBooks(result);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
     <section className={styles.catalog}>
       <h2>CATALOG</h2>
-      <ul>
-        {books.map((b) => (
-          <CatalogItem key={b._id} {...b} />
-        ))}
-      </ul>
-      {books.length === 0 && <h3>No added books!</h3>}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {books?.length > 0 ? (
+            <ul>
+              {books.map((b) => (
+                <CatalogItem key={b._id} {...b} />
+              ))}
+            </ul>
+          ) : (
+            <h3 className="no-articles">No books added yet</h3>
+          )}
+        </>
+      )}
     </section>
   );
 }
+
--- client/src/components/Catalog/CatalogItem.jsx
+++ client/src/components/Catalog/CatalogItem.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function CatalogItem({ _id, title, imageUrl }) {
   return (
-    <li>
-      <img src={imageUrl} alt={title} />
-      <h3>{title}</h3>
-      <Link to={`/details/${_id}`} className={styles["details-btn"]}>
-        Details
-      </Link>
-    </li>
+    <div className="allBooks">
+      <div className="allBooks-info">
+        <img src={imageUrl} alt={title} />
+        <h3>{title}</h3>
+        <Link to={`/details/${_id}`} className={styles["details-btn"]}>
+          Details
+        </Link>
+      </div>
+    </div>
   );
 }
