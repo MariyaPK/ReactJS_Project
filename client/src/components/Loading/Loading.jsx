81ba9750c8cfded5f793f30f08e9502048a45cdf
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-18 14:36:49 +0200

    fixesfixes

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ export default function Loading(){
     return(
         <div className="loading-container">
         <div className="loading-spinner">
-          <span className="loading-spinner-text">Loading</span>
+          <span className="loading-spinner-text">Seeking through books</span>
         </div>
       </div>
     )
