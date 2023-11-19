cb44df1c2ded17684aca3286e32847b8cc7cb1a3
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-19 18:49:30 +0200

    adding bookContext

--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -1,35 +1,53 @@
-import * as request from "./requester";
+import { requestFactory } from "./requester";
 
-const baseUrl = "http://localhost:3030/jsonstore/books";
+const baseUrl = "http://localhost:3030/data/books";
 
-export const getAll = async () => {
-  try {
-    const result = await request.get(baseUrl);
-    const books = Object.values(result);
-    // console.log(books)
+export const bookServiceFactory = (token) => {
+  const request = requestFactory(token);
 
+  const getAll = async () => {
+    try {
+      const result = await request.get(baseUrl);
+      const books = Object.values(result);
+      // console.log(books)
+
+      return books;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createBook = async (bookData) => {
+    const result = await request.post(baseUrl, bookData);
+
+    // console.log(result);
+
+    return result;
+  };
+
+  const getOne = async (bookID) => {
+    const result = await request.get(`${baseUrl}/${bookID}`);
+
+    // console.log(result);
+
+    return result;
+  };
+
+  const editBook = (bookID, bookData) => request.put(`${baseUrl}/${bookID}`, bookData);
+
+  const deleteBook = (bookID) => request.delete(`${baseUrl}/${bookID}`);
+
+  const getUserBooks = async (userId) => {
+    const books = await request.get(`${baseUrl}?where=_ownerId%3D%22${userId}%22`);
     return books;
-  } catch (error) {
-    console.log(error);
-  }
 };
 
-export const create = async (bookData) => {
-  const result = await request.post(baseUrl, bookData);
-
-  // console.log(result);
-
-  return result;
+return {
+  getAll,
+    getOne,
+    createBook,
+    editBook,
+    deleteBook,
+    getUserBooks
 };
-
-export const getOne = async (bookID) => {
-  const result = await request.get(`${baseUrl}/${bookID}`);
-
-  // console.log(result);
-
-  return result;
 };
-
-export const edit = (bookID, bookData) => request.put(`${baseUrl}/${bookID}`, bookData);
-
-export const deleteBook = (bookID) => request.del(`${baseUrl}/${bookID}`);
