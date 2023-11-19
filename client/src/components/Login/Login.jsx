fbcd030a98a415e410c16742d6ab8f27824704be
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-19 18:48:34 +0200

    fixing login/register

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,18 +1,59 @@
+import styles from "./Login.module.css"; // Import the CSS module
+
+import { Link } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
+
+const LoginFormKeys = {
+  Email: "email",
+  Password: "password",
+};
+
 export default function Login() {
+  const { onLoginSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      [LoginFormKeys.Email]: "",
+      [LoginFormKeys.Password]: "",
+    },
+    onLoginSubmit
+  );
+
   return (
     <section
       id="login"
-      style={{ backgroundImage: 'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")' }}
+      className={styles["login-section"]}
+      // style={{ backgroundImage: 'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")' }}
     >
-      <div className="form">
+      <div className={styles.container}>
         <h2>LOGIN</h2>
-        <form className="login-form">
-          <input type="text" name="email" id="login-email" placeholder="email" />
-          <input type="password" name="password" id="login-password" placeholder="password" />
+        <form className={styles["login-form"]} method="POST" onSubmit={onSubmit}>
+          <div className={styles["form-group"]}>
+            {/* <label htmlFor="login-email">Email:</label> */}
+            <input
+              type="text"
+              name={LoginFormKeys.Email}
+              id="login-email"
+              placeholder="Enter your email"
+              value={values[LoginFormKeys.Email]}
+              onChange={changeHandler}
+            />
+
+            {/* <label htmlFor="login-password">Password:</label> */}
+            <input
+              type="password"
+              id="login-password"
+              placeholder="Enter your password"
+              name={LoginFormKeys.Password}
+              value={values[LoginFormKeys.Password]}
+              onChange={changeHandler}
+            />
+          </div>
 
           <button type="submit">LOGIN</button>
-          <p className="message">
-            Not registered? <a href="/register">Register here</a>
+          <p className={styles.message}>
+            Not registered? <Link to="/register">Register here</Link>
           </p>
         </form>
       </div>
--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,18 +1,66 @@
+import styles from "./Register.module.css";
+
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { useForm } from "../../hooks/useForm";
+import { AuthContext } from "../../contexts/AuthContext";
+
 export default function Register() {
+  const { onRegisterSubmit } = useContext(AuthContext);
+  const { values, changeHandler, onSubmit } = useForm(
+    {
+      email: "",
+      password: "",
+      rePassword: "",
+    },
+    onRegisterSubmit
+  );
+
   return (
     <section
       id="register"
-      style={{ backgroundImage: 'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")' }}
+      className={styles["register-section"]}
+      // style={{ backgroundImage: 'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")' }}
     >
-      <div className="form">
+      <div className={styles.container}>
         <h2>REGISTER</h2>
-        <form className="register-form">
-          <input type="text" name="email" id="register-email" placeholder="Email..." />
-          <input type="password" name="password" id="register-password" placeholder="Password..." />
-          <input type="password" name="re-password" id="repeat-password" placeholder="Repeat password..." />
+        <form className={styles["register-form"]} method="POST" onSubmit={onSubmit}>
+          <div className={styles["form-group"]}>
+            {/* <label htmlFor="register-email">Email:</label> */}
+            <input
+              type="text"
+              name="email"
+              id="register-email"
+              placeholder="Email..."
+              value={values.email}
+              onChange={changeHandler}
+            />
+
+            {/* <label htmlFor="register-password">Password:</label> */}
+            <input
+              type="password"
+              name="password"
+              id="register-password"
+              placeholder="Password..."
+              value={values.password}
+              onChange={changeHandler}
+            />
+
+            {/* <label htmlFor="register-rePassword">Repeat password:</label> */}
+            <input
+              type="password"
+              name="rePassword"
+              id="repeat-password"
+              placeholder="Repeat password..."
+              value={values.rePassword}
+              onChange={changeHandler}
+            />
+          </div>
+
           <button type="submit">REGISTER</button>
-          <p className="message">
-            Already registered? <a href="/login">Login here</a>
+          <p className={styles.message}>
+            Already have an account? <Link to="/login">Login here</Link>
           </p>
         </form>
       </div>
