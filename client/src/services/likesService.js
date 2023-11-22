1a0331978f04680edfed1eaeda15b3917eaf73cb
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-22 18:24:02 +0200

    fixes like functionality

--- client/src/services/likesService.js
+++ client/src/services/likesService.js
@@ -4,25 +4,38 @@ const baseUrl = "http://localhost:3030/data";
 const request = requestFactory();
 
 export const getLikesCount = async (bookID) => {
-  const result = await request.get(`${baseUrl}/likes?where=bookID%3D%22${bookID}%22&`);
-  return result;
+  try {
+    const result = await request.get(`${baseUrl}/likes?where=bookID%3D%22${bookID}%22`);
+    return result;
+  } catch (error) {
+    console.error('Error fetching likes count:', error);
+  }
 };
 
 export const addLike = async (bookID, userId) => {
-  const result = await request.post(`${baseUrl}/likes`, { bookID, userId });
-
-  return result;
+  try {
+    const result = await request.post(`${baseUrl}/likes`, { bookID, userId });
+    return result;
+  } catch (error) {
+    console.error('Error adding like:', error);
+  }
 };
 
-export async function liked(bookID, userId) {
-  const result = await request.get(`${baseUrl}/likes?where=bookID%3D%22${bookID}%22%20and%20_ownerId%3D%22${userId}%22&count`, {
-    bookID,
-    userId,
-  });
-  return result;
-}
+export const liked = async (bookID, userId) => {
+  try {
+    const result = await request.get(`${baseUrl}/likes?where=bookID%3D%22${bookID}%22%20and%20_ownerId%3D%22${userId}%22&count`);
+    return result;
+  } catch (error) {
+    console.error('Error checking liked status:', error);
+  }
+};
 
-export async function dislike(likeId, userId) {
-  const res = await request.delete(`${baseUrl}/likes/${likeId}?where=userId%3D%22${userId}%22&`);
-  return res;
-}
+export const dislike = async (likeId, userId) => {
+  try {
+    const res = await request.delete(`${baseUrl}/likes/${likeId}?where=userId%3D%22${userId}%22`);
+    return res;
+  } catch (error) {
+    console.error('Error disliking:', error);
+  }
+};
+
