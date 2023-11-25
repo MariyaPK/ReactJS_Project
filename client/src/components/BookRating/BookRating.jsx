9d9105fabcd7400b6ed955389d6103f83c0969c1
MariyaPK <karadzhovaa.maria@gmail.com>
2023-11-25 20:55:12 +0200

    fixing rating template

--- client/src/components/BookRating/BookRating.jsx
+++ client/src/components/BookRating/BookRating.jsx
@@ -1,16 +1,13 @@
-import styles from "./BookRating.module.css";
-
-import { useState } from "react";
+import "./BookRating.css";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
-
 import { requestFactory } from "../../services/requester";
 import { useAuthContext } from "../../contexts/AuthContext";
 
 const request = requestFactory();
 const baseUrl = "http://localhost:3030/data";
 
-export default function Rate() {
+const Rate = () => {
   const [rate, setRate] = useState(0);
   const { bookID } = useParams();
   const { userId } = useAuthContext();
@@ -26,40 +23,46 @@ export default function Rate() {
         userId,
         rating: givenRating,
       });
-      console.log(bookID);
-      console.log(userId);
-      console.log(result);
+
+    //   console.log(bookID);
+    //   console.log(userId);
+    //   console.log(result);
 
       if (result.ok) {
         setRate(givenRating);
         alert(`You have successfully rated the book ${givenRating} stars!`);
       }
+
     } catch (error) {
       console.error("Error:", error);
       alert("Error saving rating. Please try again.");
     }
   };
 
+  const generateStars = (numStars) => {
+    const stars = [];
+    for (let i = numStars; i > 0; i--) {
+      stars.push(
+        <React.Fragment key={i}>
+          <input className={`star star-${i}`} id={`star-${i}`} type="radio" name="star" onClick={() => handleRating(i)} />
+          <label className={`star star-${i}`} htmlFor={`star-${i}`}></label>
+        </React.Fragment>
+      );
+    }
+    return stars;
+  };
+
   return (
-    <div className={styles.container}>
-      {[...Array(5)].map((book, index) => {
-        const givenRating = index + 1;
-        return (
-          <label key={givenRating}>
-            <input
-              className={styles.radio}
-              type="radio"
-              value={givenRating}
-              onClick={() => {
-                handleRating(givenRating);
-              }}
-            />
-            <div className={styles.rating}>
-              <FaStar color={givenRating <= rate ? "000" : "rgb(192,110,110)"} />
-            </div>
-          </label>
-        );
-      })}
+    <div className="container d-flex justify-content-center">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="stars">
+            <form method="POST">{generateStars(5)}</form>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Rate;
